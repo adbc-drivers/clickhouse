a5204a6d40d37d721957560709158848d6f82487
David Li <li.davidm96@gmail.com>
2026-02-03 12:34:22 +0900

    build: set up driver builds

--- src/ci/scripts/test.sh
+++ src/ci/scripts/test.sh
@@ -1,11 +1,11 @@
-#:schema https://raw.githubusercontent.com/adbc-drivers/dev/refs/heads/main/schema/generate-schema.json
+#!/bin/bash
 # Copyright (c) 2026 ADBC Drivers Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#         http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,10 +13,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-driver = "clickhouse"
+set -euo pipefail
 
-[lang]
-# Instead of wrapping their Rust code, use scripts to drive it
-[lang.script]
-[lang.script.build]
-lang-tools = ["rust"]
+main() {
+    # "test" or "release"
+    local -r platform="${1}"
+    # shellcheck disable=SC2034
+    local -r arch="${2}"
+
+    pushd "$(pwd)/adbc_clickhouse"
+    # Ignore doctests; they seem to fail
+    if [[ "${platform}" == "linux" ]]; then
+        cargo test --lib --bins --tests
+    else
+        # We don't spawn the Docker container on other platforms
+        cargo test --no-run
+    fi
+}
+
+main "$@"
